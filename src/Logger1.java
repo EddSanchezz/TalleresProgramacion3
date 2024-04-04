f5fea283d2959ce2f78fe7e9f4d0a30f926ef6a9
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2024-04-03 23:59:02 -0500

    LoggExercises

---
+++ src/Logger1.java
@@ -0,0 +1,21 @@
+import java.util.logging.Logger;
+public class Logger1 {
+
+        private static final Logger logger = Logger.getLogger(Logger1.class.getName());
+
+        public static void main(String[] args) {
+            int numero1 = 10;
+            int numero2 = 20;
+            int resultado = sumar(numero1, numero2);
+
+            logger.info("El resultado de sumar " + numero1 + " y " + numero2 + " es: " + resultado);
+        }
+
+        public static int sumar(int a, int b) {
+            logger.info("Iniciando la operación de suma...");
+            int suma = a + b;
+            logger.info("Operación de suma completada.");
+            return suma;
+        }
+
+}
---
+++ src/Logger3.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class Logger3 {
+
+    private static final Logger logger = Logger.getLogger(Logger3.class.getName());
+
+    public static void main(String[] args) {
+        int[][] matriz1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] matriz2 = {{7, 8}, {9, 10}, {11, 12}};
+        int[][] resultado = multiplicarMatrices(matriz1, matriz2);
+
+        logger.info("Matriz 1:");
+        mostrarMatriz(matriz1);
+        logger.info("Matriz 2:");
+        mostrarMatriz(matriz2);
+        logger.info("Resultado de la multiplicación:");
+        mostrarMatriz(resultado);
+    }
+
+    public static int[][] multiplicarMatrices(int[][] matriz1, int[][] matriz2) {
+        int filas1 = matriz1.length;
+        int columnas1 = matriz1[0].length;
+        int columnas2 = matriz2[0].length;
+        int[][] resultado = new int[filas1][columnas2];
+
+        for (int i = 0; i < filas1; i++) {
+            for (int j = 0; j < columnas2; j++) {
+                for (int k = 0; k < columnas1; k++) {
+                    resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+                }
+            }
+        }
+
+        return resultado;
+    }
+
+    public static void mostrarMatriz(int[][] matriz) {
+        for (int[] fila : matriz) {
+            logger.info(Arrays.toString(fila));
+        }
+    }
+}
