98822d799ec441d0a9a1d758b0fc2900b0eaf9e6
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2024-04-12 00:41:37 -0500

    contar las palabras de una cancion

--- src/Hashtable/Aplicacion.java
+++ src/Hashtable/Aplicacion.java
@@ -1,12 +1,12 @@
 package Hashtable;
 
-import static Hashtable.Archivo.crearArchivo;
 
 public class Aplicacion
 {
 
     public static void main(String[] args)    {
-
+        String archivo = "C:\\Users\\metzu\\Documents\\Repositorios\\TalleresProgramacion3\\src\\Hashtable\\LetraCancion.txt";
+        Archivo.contarPalabrasRepetidas(Archivo.convertirTextoEnLista(archivo));
     }
 
 
--- src/Hashtable/Archivo.java
+++ src/Hashtable/Archivo.java
@@ -1,60 +1,47 @@
 package Hashtable;
 
-import javax.swing.*;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Formatter;
+import java.util.*;
 
-public class Archivo
-{
-    public static void crearArchivo(){
-        Formatter archivo;
+public class Archivo {
 
-        try{
-            archivo = new Formatter("C:\\Users\\metzu\\Documents\\TallerArchivosFicherosJava\\Restaurants2.txt");
-            archivo.flush();
-            archivo.close();
-
-            JOptionPane.showMessageDialog(null, "el archivo se creo correctamente");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String contarPalabraMasRepetida()
-    {
-        String archivo = "C:\\Users\\metzu\\Documents\\TallerArchivosFicherosJava\\LetraCancion.txt";
-        String answer;
-        String palabraMayor = " ";
-        int counter = 0;
+    public static ArrayList<String> convertirTextoEnLista(String archivo) {
         ArrayList<String> lineas = new ArrayList<>();
 
-        try {
-            FileReader fileReader = new FileReader(archivo);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-           String linea;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(archivo))) {
+            String linea;
             while ((linea = bufferedReader.readLine()) != null) {
-                String[] palabrasDividas = listaDeTexto(linea);
-                lineas.addAll(Arrays.asList(palabrasDividas));
+                String[] palabrasDivididas = linea.split(" ");
+                lineas.addAll(Arrays.asList(palabrasDivididas));
             }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-
-        answer = "la palabra mas repetida fue " + palabraMayor + " que se repitió " + counter;
-        return answer;
+        return lineas;
     }
 
-    public static String[] listaDeTexto(String parrafo)
-    {
-        return parrafo.split(" ");
+    public static void contarPalabrasRepetidas(ArrayList<String> palabras) {
+        Map<String, Integer> contadorPalabras = new HashMap<>();
 
+        for (String palabra : palabras) {
+            contadorPalabras.put(palabra, contadorPalabras.getOrDefault(palabra, 0) + 1);
+        }
+
+        String palabraMasRepetida = "";
+        int maxRepetida = 0;
+
+        for (Map.Entry<String, Integer> entry : contadorPalabras.entrySet()) {
+            if (entry.getValue() > maxRepetida) {
+                palabraMasRepetida = entry.getKey();
+                maxRepetida = entry.getValue();
+            }
+        }
+
+        System.out.println("La palabra más repetida es: " + palabraMasRepetida);
+        System.out.println("Se repite " + maxRepetida + " veces.");
     }
 }
